31be8436639a904711ddfc32291cbe977adcea3d
marcu <dsf@asd.com>
2020-09-21 16:19:52 +0200

    Initial commit

---
+++ src/Regnemaskine.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Regnemaskine{
+
+    public static void main (String[] args) {
+        int result=0;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Skriv det tegn du vil bruge (+, -, *, /)");
+        //System.out.println("" + input.nextLine());
+        String tegn = input.nextLine();
+
+        System.out.println("Skriv det først tal");
+        int a = input.nextInt();
+
+        System.out.println("Skriv det næste tal");
+        int b = input.nextInt();
+        input.close();
+
+        if (tegn.equals("+")){
+            result = a + b;
+        }
+        if (tegn.equals("-")){
+            result = a - b;
+        }
+        if (tegn.equals("*")){
+            result = a * b;
+        }
+        if (tegn.equals("/")){
+            result = a / b;
+        }
+
+        System.out.println("Resultat: " + a + tegn + b + " = " + result);
+    }
+}
